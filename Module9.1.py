33668ceb65053a61d1809370336bea6aef95147f
nurn-alam <nurn@metropolia.fi>
2025-10-26 19:10:07 +0600

    Fix formatting issues in Car class and print statements

--- Module9.1.py
+++ Module9.1.py
@@ -5,11 +5,10 @@ class Car:
         self.current_speed = 0
         self.travelled_distance = 0
 
-
 car = Car("ABC-123", 142)
-print("Car status check..")
+print("Car status check...")
 print(f"Registration number: {car.reg_number:s}")
 print(f"Maximum speed: {car.max_speed:d} km/h")
 print(f"Current speed: {car.current_speed:d} km/h")
-
 print(f"Travel distance: {car.travelled_distance:d} km")
+
