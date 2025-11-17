5065eaa4bfe2733fcf79bdfbc4831d5d06870c0a
nurn-alam <nurn@metropolia.fi>
2025-11-17 23:40:17 +0400

    Refactor car race simulation into Car class

--- Module9.4.py
+++ Module9.4.py
@@ -1,24 +1,23 @@
 import random
 
+class Car:
+        def __init__(self, reg_number, max_speed):
+                self.reg_number = reg_number
+                self.max_speed = max_speed
+                self.current_speed = 0
+                self.travelled_distance = 0
+        
+        def accelerate(self, speed_change):
+                self.current_speed += speed_change
+                if self.current_speed < 0:
+                        self.current_speed = 0
+                elif self.current_speed > self.max_speed:
+                        self.current_speed = self.max_speed
+                
+        def drive(self, hours):
+                self.travelled_distance += self.current_speed * hours
 
-def __init__(self, reg_number, max_speed):
-        self.reg_number = reg_number
-        self.max_speed = max_speed
-        self.current_speed = 0
-        self.travelled_distance = 0
-
-def accelerate(self, speed_change):
-        self.current_speed += speed_change
-        if self.current_speed < 0:
-            self.current_speed = 0
-        elif self.current_speed > self.max_speed:
-            self.current_speed = self.max_speed
-
-def drive(self, hours):
-        distance_driven = self.current_speed * hours
-        self.travelled_distance += self.current_speed + hours
-
-
+# Race cars
 race_cars = []
 for i in range(1, 11):
     max_speed = random.randint(100, 200)
@@ -36,7 +35,7 @@ while race_continue:
         car.accelerate(random.randint(-10, 15))
         car.drive(1)
 
-        if car.distance_travelled >= 10000:
+        if car.travelled_distance >= 10000:
             race_continue = False
             winner = car
 
@@ -45,4 +44,4 @@ print(f"The winner is {winner.reg_number}!")
 print(f"{'Reg Number':<14}{'Max Speed':>10}{'Current Speed':>18}{'Distance':>11}")
 for car in race_cars:
     print(f"{car.reg_number:<12}{car.max_speed:>10}"
-          f"{car.current_speed:>15}{car.distance_travelled:>15.1f}")
+          f"{car.current_speed:>15}{car.travelled_distance:>15.1f}")
